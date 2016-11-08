29af308c5162ee1ab4c07bbd9be25a2fbe90a7f3
Zach McCleaf <zmccleaf@andculture.com>
2016-11-08 13:07:10 -0500

    fixing port

--- app/server/server.js
+++ app/server/server.js
@@ -6,34 +6,8 @@ Session = require('express-session');
 
 app = express();
 
-port = 3000;
+port = port.env.PORT || 3000;
 
 path = require('path');
-
-app.use(Session({
-  secret: 'thisisasecret'
-}));
-
-app.use(function(err, request, response, next) {
-  console.log(err);
-  return response.status(500).send('Something broke!');
-});
-
-app.get('/', function(request, response) {
-  var b, sess;
-  sess = request.session;
-  return b = Board.load('./assets/boards/board_1.json', function(err, board) {
-    if (err) {
-      throw new Error('Bjorked');
-    }
-    return response.render('home', board);
-  });
-});
-
-app.listen(port, function(err) {
-  if (err) {
-    return console.log('something bad happened', err);
-  }
-});
 
 console.log("server is listening on " + port);
